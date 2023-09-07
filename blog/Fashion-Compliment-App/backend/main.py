aa818fc7abcf4bd7c358f601dea490923553a75c
Etsuji Nakai <enakai0@gmail.com>
2023-09-07 09:17:29 +0000

    Update

---
+++ blog/Fashion-Compliment-App/backend/main.py
@@ -0,0 +1,70 @@
+import base64
+import json
+import os
+
+from flask import Flask, request
+from vertexai.preview.language_models import TextGenerationModel
+from vertexai.preview.vision_models import ImageQnAModel, ImageCaptioningModel, Image
+
+generation_model = TextGenerationModel.from_pretrained('text-bison@001')
+image_captioning_model = ImageCaptioningModel.from_pretrained('imagetext@001')
+image_qna_model = ImageQnAModel.from_pretrained('imagetext@001')
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'GenAI fashion compliment service.'
+
+def get_image_description(image):
+    results = image_captioning_model.get_captions(
+        image=image,
+        number_of_results=3)
+    results.sort(key=len)
+    return results[-1]
+
+
+def get_fashion_items(image):
+    results = image_qna_model.ask_question(
+      image=image,
+      question='details of the fashion items in this picture.',
+      number_of_results=3)
+    results.sort(key=len)
+    return results[-1]
+
+
+def get_compliment_message(image):
+    prompt = '''\
+ファッションアドバイザーの立場で、以下の様に記述される人物を褒め称える文章を作ってください。
+その人物に語りかける様に、数行の文章を作ってください。
+個人を特定する名前は使用しないでください。
+
+記述：{}
+
+ファッションアイテム：{}
+'''
+
+    description = get_image_description(image)
+    items = get_fashion_items(image)
+    answer = generation_model.predict(
+        prompt.format(description, items),
+        temperature=0.2, max_output_tokens=1024,
+        top_k=40, top_p=0.8).text
+    return answer 
+
+
+@app.route('/fashion-compliment-service/api/v1/get-compliment', methods=['POST'])
+def grammar_correction():
+    json_data = request.get_json()
+    image_base64 = json_data['image']
+    image = Image(base64.b64decode(image_base64))
+    message = get_compliment_message(image)
+    resp = {'message': message}
+
+    return resp, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
---
+++ blog/Fashion-Compliment-App/src/App.js
@@ -0,0 +1,178 @@
+import React, { useState, useRef, useEffect } from 'react';
+import "./App.css";
+import loading from "./images/loading.gif";
+
+const projectId = "etsuji-genai-demo";
+
+
+
+
+class FileReaderEx extends FileReader{
+    #readAs(blob, ctx){
+        return new Promise((res, rej)=>{
+            super.addEventListener("load", ({target}) => res(target.result));
+            super.addEventListener("error", ({target}) => rej(target.error));
+            super[ctx](blob);
+        });
+    }
+
+    readAsDataURL(blob){
+        return this.#readAs(blob, "readAsDataURL");
+    }
+}
+
+
+export const App = (props) => {
+
+  const chatDataInit = [{
+    "user": "bot",
+    "text": "今日のあなたのファッションは？"
+  }];
+
+  const messageEnd = useRef(null);
+  const inputRef = useRef(null);
+  const [chatData, setChatData] = useState(chatDataInit);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const resizeImage = async (imageData, width) => {
+    const context = document.createElement('canvas').getContext('2d')
+
+    const image = await new Promise((resolve, reject) => {
+      const image = new Image();
+      image.addEventListener('load', () => resolve(image));
+      image.addEventListener('error', reject);
+      image.src = URL.createObjectURL(imageData);
+    })
+    const { naturalHeight: heightCurrent, naturalWidth: widthCurrent } = image;
+
+    const height = heightCurrent * (width / widthCurrent);
+    context.canvas.width = width;
+    context.canvas.height = height;
+    context.drawImage(image, 0, 0, widthCurrent, heightCurrent, 0, 0, width, height);
+
+    return new Promise((resolve) => {
+      context.canvas.toBlob(resolve, `image/jpeg`, 0.9)
+    })
+  }
+
+  const onFileInputChange = async (evt) => {
+    setButtonDisabled(true);
+
+    const image = await resizeImage(evt.target.files[0], 500);
+
+    let chatDataNew = chatData.concat();
+    chatDataNew.push({"user": "image", "image": image});
+    chatDataNew.push({"user": "bot", "text": "_typing_"});
+    setChatData(chatDataNew);
+
+    const data = await getAnswer(image);
+
+    chatDataNew.pop();
+    chatDataNew.push({"user": "bot", "text": data.message});
+    setChatData(chatDataNew);
+
+    setButtonDisabled(false);
+  };
+
+  // eslint-disable-next-line
+  const getAnswer = async (image) => {
+
+// eslint-disable-next-line
+    const callBackend = async (imageBase64) => {
+      const baseURL = "https://" + projectId + ".web.app";
+      const apiEndpoint = baseURL + "/fashion-compliment-service/api/v1/get-compliment";
+      const request = {  
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "image": imageBase64,
+        })
+      };
+      const res = await fetch(apiEndpoint, request);
+      const data = await res.json();
+      return data;
+    };
+
+    let imageBase64 = await new FileReaderEx().readAsDataURL(image);
+    imageBase64 = imageBase64.replace("data:", "").replace(/^.+,/, "");
+    const data = await callBackend(imageBase64);
+    return data
+  }
+
+  const chatBody = [];
+  let i = 0;
+  for (const item of chatData) {
+    i += 1;
+
+    if (item.user === "bot") {
+      let elem;
+      if (item.text === "_typing_") {
+        elem = (
+          <div key={i} className="typing">
+            <img src={loading} alt="loading" style={{
+              width: "100px",
+              marginLeft: "150px"}}/>
+          </div>
+        );          
+      } else {
+        elem = (
+          <div key={i} className="bot" style={{
+            width: "300px",
+            padding: "10px",
+            marginBottom: "20px",
+            border: "1px solid #333333",
+            borderRadius: "10px"}}> {item.text}
+          </div>
+        );
+      };
+      chatBody.push(elem);
+    }
+
+    if (item.user === "image") {
+      const imageURL = window.URL.createObjectURL(item.image);
+      const elem = (
+        <div key={i} className="image" align="right">
+          <img src={imageURL} width="200" alt="user provided"/>
+        </div>
+      );
+      chatBody.push(elem);
+    }
+  }
+
+  if (buttonDisabled === false) {
+    const elem = (
+      <div key="fileUpload" className="fileUpload" align="right">
+        <button
+          onClick={() => inputRef.current.click()}>ファイルアップロード</button>
+        <input
+          hidden ref={inputRef} type="file" accept="image/*"
+          onChange={onFileInputChange}/>
+      </div>            
+    );
+    chatBody.push(elem);
+  }
+
+  const element = (
+        <>
+        <div className="App">
+          {chatBody}
+        </div>
+        <div ref={messageEnd} />
+        </>
+  );
+
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+  useEffect(() => {
+    const scrollUp = async () => {
+      messageEnd.current?.scrollIntoView();
+      await sleep(500);
+      messageEnd.current?.scrollIntoView();
+    };
+    scrollUp();
+  });
+
+  return element;
+}
