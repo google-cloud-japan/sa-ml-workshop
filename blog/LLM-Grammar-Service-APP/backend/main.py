18211e2fcbc38fa61c9f98c59f8e188240aecb7b
Etsuji Nakai <enakai0@gmail.com>
2024-01-11 03:01:44 +0900

    Update main.py

--- blog/LLM-Grammar-Service-APP/backend/main.py
+++ blog/LLM-Grammar-Service-APP/backend/main.py
@@ -59,4 +59,4 @@ answer:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
