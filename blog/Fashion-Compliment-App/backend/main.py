2e580f24a10a3b258f029c8a1a0861dd04487aad
Etsuji Nakai <enakai0@gmail.com>
2024-01-11 03:02:10 +0900

    Update main.py

--- blog/Fashion-Compliment-App/backend/main.py
+++ blog/Fashion-Compliment-App/backend/main.py
@@ -97,4 +97,4 @@ def fashion_compliment():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
